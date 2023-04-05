eede5fa95d3748b0dfb3fcd4f28fef69fd4ce184
Dmitry <borodulkin_d@mail.ru>
2023-04-05 17:24:28 +0300

    Created @IBOutlets and @IBActions.

--- NewColorizedApp/ViewControllers/SettingViewController.swift
+++ NewColorizedApp/ViewControllers/SettingViewController.swift
@@ -9,21 +9,43 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IB Actions
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        
     }
-    */
+    
+    
+    @IBAction func doneButtonPressed() {
+  
+    }
+    
+    
+
 
 }
